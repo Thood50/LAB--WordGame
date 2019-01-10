c105a84f399ea1140e11e0f19e497d97dead7a56
Tyler Hood <tyler.r.hood@gmail.com>
2019-01-10 10:54:11 -0800

    Able to create new text file

--- WordGame/WordGame/Program.cs
+++ WordGame/WordGame/Program.cs
@@ -8,7 +8,26 @@ namespace WordGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = "../../../newFile.txt";
+            CreateTextFile(path);
+        }
+
+        static void CreateTextFile(string location)
+        {
+            //Need a try catch to handle any possible Exceptions
+            try
+            {
+                //Putting streamwriter in a using block so that any files that are touched/opened get
+                // closed at the end of the block
+                using (StreamWriter createNewFile = new StreamWriter(location))
+                {
+                    createNewFile.WriteLine("Im writing in a new file");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
